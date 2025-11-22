fdf143eb3effe071e442041ec78734e9e0129def
ClassicMan <legenderyprime@gmail.com>
2025-11-22 18:16:29 +0100

    Update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) {
+  throw new Error('VITE_SUPABASE_URL is missing')
+}
+if (!supabaseAnonKey) {
+  throw new Error('VITE_SUPABASE_ANON_KEY is missing')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
